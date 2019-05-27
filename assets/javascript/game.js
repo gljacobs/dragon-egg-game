c48ea036a685b9dc3d94553717a8b490c71b2579
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-05-26 21:46:58 -0700

    add logic for win/lose scenarios and reset game appropriately

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
             this.gold = 0;
 
             $("#goal").text(this.goal);
+            $("#score").text(this.gold);
+
             $("#green-egg").val("1")
             $("#red-egg").val("" + Math.floor(Math.random() * 10 + 10));
             $("#gold-egg").val("" + Math.floor(Math.random() * 10 + 20));
@@ -24,5 +26,17 @@ $(document).ready(function() {
     $(".egg").on("click", function(){
         game.gold += parseInt($(this).val());
         $("#score").text(game.gold);
+        if(game.gold === game.goal){
+            game.wins++;
+            $("#wins").text("Wins: " + game.wins);
+            game.reset();
+            alert("Congrats, you are quite the Dragon Egg Hunter!")
+        }
+        if(game.gold > game.goal){
+            game.losses++;
+            $("#losses").text("Losses: " + game.losses);
+            game.reset();
+            alert("You lose, it seems like you over did it hunting for Dragon Eggs.")
+        }
     })
 });
