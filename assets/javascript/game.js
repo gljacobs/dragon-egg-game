562ceb1f3518e6b22bac49f11ece4ebe7093d054
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-05-26 21:36:48 -0700

    set up game logic regarding clickng eggs for gold and adding to total gold

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,14 @@ $(document).ready(function() {
         wins: 0,
         losses: 0,
         reset: function(){
-            this.goal = Math.floor(Math.random() * 150 + 1);
+            this.goal = Math.floor(Math.random() * 50 + 100);
             this.gold = 0;
 
+            $("#goal").text(this.goal);
             $("#green-egg").val("1")
-            $("#red-egg").val("" + Math.floor(Math.random() * 10 + 10))
-            $("#gold-egg").val("" + Math.floor(Math.random() * 10 + 20))
-            $("#mystic-egg").val("" + Math.floor(Math.random() * 10 + 30))
+            $("#red-egg").val("" + Math.floor(Math.random() * 10 + 10));
+            $("#gold-egg").val("" + Math.floor(Math.random() * 10 + 20));
+            $("#mystic-egg").val("" + Math.floor(Math.random() * 10 + 30));
         }
     }
 
@@ -20,5 +21,8 @@ $(document).ready(function() {
 
     console.log($("#red-egg").val());
     
-
+    $(".egg").on("click", function(){
+        game.gold += parseInt($(this).val());
+        $("#score").text(game.gold);
+    })
 });
